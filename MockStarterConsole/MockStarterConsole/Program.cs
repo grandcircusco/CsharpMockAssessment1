06559eeea526cbeda2c0c27e1257d31fc3235143
Tommy Waalkes <Tommy@grandcircus.co>
2020-07-24 13:24:59 -0400

    Added starter code

---
+++ MockStarterConsole/MockStarterConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MockStarterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
